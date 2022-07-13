2edd3cb6b80f9f081f227de3fe09530db434277b
arash <kariznoviarash@gmail.com>
2022-07-13 13:59:29 +0430

    updated

---
+++ precache-manifest.43dd78527623061ea434e6abba3c804c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16970fda58aa0a9ab7b93d3fe45b4e81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2fb767b19171787096",
+    "url": "/static/css/main.9332181d.chunk.css"
+  },
+  {
+    "revision": "1070e63c52d662bdfedc",
+    "url": "/static/js/2.f1752b88.chunk.js"
+  },
+  {
+    "revision": "fd2fb767b19171787096",
+    "url": "/static/js/main.28795916.chunk.js"
+  },
+  {
+    "revision": "51810371bf2cf35f1e22",
+    "url": "/static/js/runtime-main.85498786.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c520fca4422142ed52a8c93a06877f5d.js"
+  "/precache-manifest.43dd78527623061ea434e6abba3c804c.js"
 );
 
 self.addEventListener('message', (event) => {
